382679ae0822e992faa29fbc37f41198de71e7bb
Pablo Sebastian Vargas Grateron <pablo.vargasgrateron@studio.unibo.it>
2024-03-25 12:10:52 +0100

    Done ex1

--- src/main/scala/tasks/adts/Ex1ComplexNumber.scala
+++ src/main/scala/tasks/adts/Ex1ComplexNumber.scala
@@ -2,6 +2,11 @@ package tasks.adts
 
 package u04lab
 
+import scala.compiletime.ops.int
+import u02.Values.s
+import u04.typeclasses.HigherKindedTypes.s
+import scala.compiletime.ops.string
+
 /*  Exercise 1: 
  *  Complete the implementation of ComplexADT trait below, so that it passes
  *  the test in ComplexTest.
@@ -22,11 +27,17 @@ object Ex1ComplexNumbers:
   object BasicComplexADT extends ComplexADT:
 
     // Change assignment below: should probably define a case class and use it?
-    type Complex = Nothing 
-    def complex(re: Double, im: Double): Complex = ???
+    case class BasicComplex(re: Double, im: Double)
+    type Complex = BasicComplex
+    def complex(re: Double, im: Double): Complex = BasicComplex(re, im)
     extension (complex: Complex)
-      def re(): Double = ???
-      def im(): Double = ???
-      def sum(other: Complex): Complex = ???
-      def subtract(other: Complex): Complex = ???
-      def asString(): String = ???
+      def re(): Double = complex.re
+      def im(): Double = complex.im
+      def sum(other: Complex): Complex = BasicComplex(complex.re() + other.re(), complex.im() + other.im())
+      def subtract(other: Complex): Complex = BasicComplex(complex.re() - other.re(), complex.im() - other.im())
+      def asString(): String = complex match
+        case complex if (re == 0 & im == 0) => s"$re"
+        case complex if (re == 0) => s"$im"+"i"
+        case complex if (im == 0) => s"$re"
+        case complex if (re != 0 & im > 0) => s"$re + $im"+"i"
+        case complex if (re != 0 & im < 0) => s"$re - ${-1*im}"+"i"
